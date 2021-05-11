fc2eb29d2150b517f76ae91c4ae6cc899c4829fe
三浦 <miura.mai910@gmail.com>
2021-05-12 03:31:34 +0900

    削除機能を実装　showメソッドの誤字を修正

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,7 @@ class PostsController < ApplicationController
   end
 
   def show
-    @post = Post.find(parmas[:id])
+    @post = Post.find(params[:id])
   end
 
   def new
@@ -23,6 +23,9 @@ class PostsController < ApplicationController
   end
 
   def destroy
+    post = Post.find(params[:id])
+    post.destroy!
+    redirect_to root_path
   end
 
   def post_params
