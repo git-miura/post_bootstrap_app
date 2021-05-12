6232199e5cde9eba08763dd140d64bde9305161d
三浦 <miura.mai910@gmail.com>
2021-05-12 16:26:38 +0900

    コントローラのエラー、誤字の修正

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :set_post, only %i[show edit update destroy]
+  before_action :set_post, only: %i[show edit update destroy]
 
   def index
     @posts = Post.order(id: :asc)
@@ -22,12 +22,12 @@ class PostsController < ApplicationController
   end
 
   def update
-    ＠post.update!(post_params)
+    @post.update!(post_params)
     redirect_to ＠post, notice: "更新しました"
   end
 
   def destroy
-    ＠post.destroy!
+    @post.destroy!
     redirect_to root_path, alert: "削除しました"
   end
 
